657ef0fd2d4af5038ab60410967e8927ed0f78af
mskurnickcode <mskurnickcode@gmail.com>
2020-11-16 19:20:43 +0200

    Changes from 11/16: Search Function and Entry Page with markup added

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'wiki'
 urlpatterns = [
-    path("wiki", views.wiki, name="wiki"),
+    path("wiki/<str:entry>", views.entry, name="entry"),
+    path("search/", views.search, name="search"),
     path("", views.index, name="index")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,13 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from markdown2 import markdown
+from django import forms
 from . import util
+
+class SearchForm(forms.Form):
+    search = forms.CharField(label="Search", required= False)
+    widget = forms.TextInput(attrs={'placeholder':'Search Encyclopedia'})
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
-def wiki(request):
+def entry(request, entry):
+    page = util.get_entry(entry)
+    if page is None:
+        return render(request, "encyclopedia/entry.html", {
+        "entry": "Does not exist"
+    })
+    
+    page = markdown(page)
+    return render(request, "encyclopedia/entry.html", {
+        "entry": entry,
+        "content": page
+    })
 
-    return render(request, "encyclopedia/wiki.html")
+def search(request):
+    query = request.GET['q']
+    if util.get_entry(query):
+        return HttpResponseRedirect(reverse("wiki:entry", args=(query,)))
+
+    else:
+        return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+
 
