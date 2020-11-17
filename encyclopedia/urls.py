d0f27404b3896eaf8b13d665b8c06e149468cbd7
mskurnickcode <mskurnickcode@gmail.com>
2020-11-17 23:44:58 +0200

    Added in Create New Page path and feature

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'wiki'
 urlpatterns = [
+    path("new/", views.new, name = "new"),
     path("wiki/<str:entry>", views.entry, name="entry"),
     path("search/", views.search, name="search"),
     path("", views.index, name="index")
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,6 +9,11 @@ class SearchForm(forms.Form):
     search = forms.CharField(label="Search", required= False)
     widget = forms.TextInput(attrs={'placeholder':'Search Encyclopedia'})
 
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="Title", required=True)
+    content = forms.CharField(widget=forms.Textarea)
+
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
@@ -36,6 +41,36 @@ def search(request):
         return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def new(request):
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+
+            entries = util.list_entries()
+            if title in entries:
+                return render(request, "encyclopedia/error.html", {
+                    "error": "Entry Exists",
+                    "text": "Please edit the entry through it's page"
+                })
+
+            else:
+                markdown_title = "#" + title
+                new_content = markdown_title + "\n" + content
+                util.save_entry(title, new_content)
+                entry = util.get_entry(title)
+                page = markdown(entry)
+                return render(request, "encyclopedia/entry.html", {
+                "entry": title,
+                "content": page
+                })
+
+    else:
+        return render(request, "encyclopedia/new.html", {
+            "form": NewEntryForm()
+        })
 
 
 
